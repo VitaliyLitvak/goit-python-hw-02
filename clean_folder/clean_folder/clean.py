46729a5eb743f6f9ce50e845eb00841ec919cc5e
VitaliyLitvak <vitaliy.litvak@gmail.com>
2023-06-08 17:34:10 +0200

    Update clean.py

--- clean_folder/clean_folder/clean.py
+++ clean_folder/clean_folder/clean.py
@@ -8,6 +8,11 @@ TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", 
                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
 TRANS = {}
 
+path = Path.cwd()
+usr_path = input(f'Enter the path to sort or press Enter for the current directory {path}: ')
+path = usr_path if usr_path else path
+path = sys.argv[1]
+
 def normalize(file_name):
     file_name = sub(r"\W", "_", file_name)
     for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
@@ -59,17 +64,13 @@ def sorter(path):
                 item.rmdir()
         elif item.is_file():
             proccessing(item)
-
+            
 def main():
     path = Path.cwd()
     usr_path = input(f'Enter the path to sort or press Enter for the current directory {path}: ')
     path = usr_path if usr_path else path
-    print(path)
-    return path
+    path = sys.argv[1]
+    sorter(Path(path))
  
 if __name__ == "__main__" :
-    if len(sys.argv) > 1:
-        path_arg = sys.argv[1]
-        sorter(Path(path_arg))
-    else:
-        main()
+    main()
