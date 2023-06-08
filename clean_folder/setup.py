5a247c673bb7e170ab4d6873bc6db01fcd67c135
VitaliyLitvak <vitaliy.litvak@gmail.com>
2023-06-08 02:35:46 +0200

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_namespace_packages
 setup(name='clean-folder',
       version='1',
       description='File sorter',
-      url='https://github.com/maxafanasiev/homework_2',
+      url='https://github.com/VitaliyLitvak/goit-python-hw-02',
       author='Litvak Vitaliy',
-      author_email='afanasievmaksym2@gmail.com',
+      author_email='vitaliy.litvak@gmail.com',
       packages=find_namespace_packages(),
       entry_points={'console_scripts': ['clean-folder = clean_folder.clean:sorter']})
-
